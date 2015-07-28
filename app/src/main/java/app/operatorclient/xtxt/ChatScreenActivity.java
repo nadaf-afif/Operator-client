0de7667bd5afcab25a90e5111dc1a9e93e46fc8c
mac <kiranboghra@gmail.com>
2015-07-28 21:37:49 +0530

    capital letter

--- app/src/main/java/app/operatorclient/xtxt/ChatScreenActivity.java
+++ app/src/main/java/app/operatorclient/xtxt/ChatScreenActivity.java
@@ -219,7 +219,7 @@ public class ChatScreenActivity extends Activity implements RequestManger.Consta
 
             for (int i = 0; i < arr.length; i++) {
                 sb.append(Character.toUpperCase(arr[i].charAt(0)))
-                        .append(arr[i].substring(1)).append(" ");
+                        .append(arr[i].substring(1).toLowerCase()).append(" ");
             }
             return sb.toString().trim();
         } catch (Exception e) {
